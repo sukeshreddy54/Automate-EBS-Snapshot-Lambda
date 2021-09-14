b4049dc55e45a6d9d9991ca2d45a5b2a480f7a0f
sukeshreddy54 <77787866+sukeshreddy54@users.noreply.github.com>
2021-09-14 19:30:27 +0530

    Update snapshot-creation.py

--- snapshot-creation.py
+++ snapshot-creation.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
             for r in reservations
         ], [])
  
-    print "Found %d instances that need backing up" % len(instances)
+    print ("Found %d instances that need backing up" % len(instances))
  
     to_tag = collections.defaultdict(list)
  
@@ -35,8 +35,8 @@ def lambda_handler(event, context):
             if dev.get('Ebs', None) is None:
                 continue
             vol_id = dev['Ebs']['VolumeId']
-            print "Found EBS volume %s on instance %s" % (
-                vol_id, instance['InstanceId'])
+            print ("Found EBS volume %s on instance %s" % (
+                vol_id, instance['InstanceId']))
  
             snap = ec.create_snapshot(
                 VolumeId=vol_id,
@@ -44,18 +44,18 @@ def lambda_handler(event, context):
  
             to_tag[retention_days].append(snap['SnapshotId'])
  
-            print "Retaining snapshot %s of volume %s from instance %s for %d days" % (
+            print ("Retaining snapshot %s of volume %s from instance %s for %d days" % (
                 snap['SnapshotId'],
                 vol_id,
                 instance['InstanceId'],
                 retention_days,
-            )
+            ))
  
  
     for retention_days in to_tag.keys():
         delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
         delete_fmt = delete_date.strftime('%Y-%m-%d')
-        print "Will delete %d snapshots on %s" % (len(to_tag[retention_days]), delete_fmt)
+        print ("Will delete %d snapshots on %s" % (len(to_tag[retention_days]), delete_fmt))
         ec.create_tags(
             Resources=to_tag[retention_days],
             Tags=[
